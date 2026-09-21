b8aca10ddf480769ae373137d98d84c15e47de7f
Robin K. S. Hankin <hankin.robin@gmail.com>
2026-09-21 08:30:02 +0000

    version 1.0-6-1

--- R/type_1.R
+++ R/type_1.R
@@ -1,18 +1,18 @@
 ## real symmetric matrices ("rsm"); setClass("real_symmetric_matrix") in 'aaa_allclasses.R'
 
 
-`real_symmetric_matrix` <- function(M){new("real_symmetric_matrix",x=cbind(M))}  
+`real_symmetric_matrix` <- function(M){new("real_symmetric_matrix", x=cbind(M))}  
 ## previous line is the only place new("real_symmetric_matrix",...) is called
 
-`is.real_symmetric_matrix` <- function(x){inherits(x,"real_symmetric_matrix")}
+`is.real_symmetric_matrix` <- function(x){inherits(x, "real_symmetric_matrix")}
 
-`r_to_n_rsm` <- function(r){(sqrt(1+4*r)-1)/2}
-`n_to_r_rsm` <- function(n){n*(n+1)/2}
+`r_to_n_rsm` <- function(r){(sqrt(1+4*r)-1) / 2}
+`n_to_r_rsm` <- function(n){n*(n+1) / 2}
 
 `is_ok_rsm` <- function(r){ # 'r' = number of rows in [rowwise] matrix
-    jj <- sqrt(1+8*r)
+    jj <- sqrt(1 + 8*r)
     if(jj == round(jj)){
-        return((jj+1)/2) # size of nxn real matrix
+        return((jj+1) / 2) # size of nxn real matrix
     } else {
         stop("not correct")
     }
@@ -33,7 +33,7 @@
 
 setValidity("real_symmetric_matrix", valid_rsm)
 
-`as.real_symmetric_matrix` <- function(x,d,single=FALSE){  # single modelled on as.onion()
+`as.real_symmetric_matrix` <- function(x, d, single=FALSE){  # single modelled on as.onion()
     if(is.real_symmetric_matrix(x)){
         return(x)
     } else if(is.matrix(x)){
@@ -42,75 +42,73 @@ setValidity("real_symmetric_matrix", valid_rsm)
         if(single){
             return(real_symmetric_matrix(x))
         } else {
-            return(numeric_to_real_symmetric_matrix(x,d)) # problem! we do not know how big it is
+            return(numeric_to_real_symmetric_matrix(x, d)) # problem! we do not know how big it is
         }
     } else {
         stop("not recognised")
     }
 }
 
-`numeric_to_real_symmetric_matrix` <- function(x,d){stop("no unique coercion")}
-`rrsm` <- function(n=3,d=5){real_symmetric_matrix(matrix(round(rnorm(n*(d*(d+1)/2)),2),ncol=n))}
-`rsm_id` <- function(n,d){as.real_symmetric_matrix(kronecker(rsm1_to_vec(diag(nrow=d)),t(rep(1,n))))}
+`numeric_to_real_symmetric_matrix` <- function(x, d){stop("no unique coercion")}
+`rrsm` <- function(n=3, d=5){real_symmetric_matrix(matrix(round(rnorm(n*(d*(d+1)/2)), 2), ncol=n))}
+`rsm_id` <- function(n, d){as.real_symmetric_matrix(kronecker(rsm1_to_vec(diag(nrow=d)), t(rep(1, n))))}
 
 `vec_to_rsm1` <- function(x){
    r <- length(x)
-   n <- (sqrt(1+8*r)-1)/2
-   stopifnot(n==round(n))
-   out <- matrix(0,n,n)
-   out[upper.tri(out,TRUE)] <- x
+   n <- (sqrt(1+8*r)-1) / 2
+   stopifnot(n == round(n))
+   out <- matrix(0, n, n)
+   out[upper.tri(out, TRUE)] <- x
    out <- out + t(out)
-   diag(out) <- diag(out)/2
+   diag(out) <- diag(out) / 2
    return(out)
 }
 
-`rsm1_to_vec` <- function(M){M[upper.tri(M,TRUE)]}
+`rsm1_to_vec` <- function(M){M[upper.tri(M, TRUE)]}
 
-
-
-`vec_rsmprod_vec` <- function(x,y){
+`vec_rsmprod_vec` <- function(x, y){
     x <- vec_to_rsm1(x)
     y <- vec_to_rsm1(y)
-    jj <- (cprod(x,y)+cprod(y,x))/2
+    jj <- (cprod(x,y)+cprod(y, x)) / 2
     return(rsm1_to_vec(jj))
 }
 
-setMethod("as.1matrix","real_symmetric_matrix",function(x,drop=TRUE){
-    out <- lapply(seq_along(x), function(i){x[i,drop=TRUE]})
+setMethod("as.1matrix", "real_symmetric_matrix", function(x, drop=TRUE){
+    out <- lapply(seq_along(x), function(i){x[i, drop=TRUE]})
     if((length(x)==1) & drop){out <- out[[1]]}
     return(out)
 } )
 
-`rsm_prod_rsm`  <- function(e1,e2){
-    jj <- harmonize_oo(e1,e2)
+`rsm_prod_rsm`  <- function(e1, e2){
+    jj <- harmonize_oo(e1, e2)
     out <- jj[[1]]*0
     for(i in seq_len(ncol(out))){
-        out[,i] <- vec_rsmprod_vec(jj[[1]][,i],jj[[2]][,i])
+        out[,i] <- vec_rsmprod_vec(jj[[1]][,i], jj[[2]][,i])
     }
-    return(as.jordan(out,e1))
+    return(as.jordan(out, e1))
 }
 
 `rsm_inverse` <- function(e1){
     out <- as.matrix(e1)
     for(i in seq_len(ncol(out))){
-        out[,i] <- rsm1_to_vec(solve(e1[i,drop=TRUE])) # the meat
+        out[,i] <- rsm1_to_vec(solve(e1[i, drop=TRUE])) # the meat
     }
-    return(as.jordan(out,e1))
+    return(as.jordan(out, e1))
 }
 
-`rsm_power_numeric` <- function(e1,e2){
-    jj <- harmonize_oo(e1,e2)
+`rsm_power_numeric` <- function(e1, e2){
+    jj <- harmonize_oo(e1, e2)
     out <- jj[[1]]*0
     for(i in seq_len(ncol(out))){
-        out[,i] <- rsm1_to_vec(mymatrixpower(vec_to_rsm1(jj[[1]][,i]),jj[[2]][i])) # the meat
+        out[,i] <- rsm1_to_vec(mymatrixpower(vec_to_rsm1(jj[[1]][,i]), jj[[2]][i])) # the meat
     }
     return(as.jordan(out,e1))
 }
 
-`rsm_arith_rsm` <- function(e1,e2){
+`rsm_arith_rsm` <- function(e1, e2){
   switch(.Generic,
          "+" = jordan_plus_jordan(e1, e2),
-         "-" = jordan_plus_jordan(e1,jordan_negative(e2)),
+         "-" = jordan_plus_jordan(e1, jordan_negative(e2)),
          "*" = rsm_prod_rsm(e1, e2),
          "/" = rsm_prod_rsm(e1, rsm_inverse(e2)), 
          "^" = stop("rsm^rsm not defined"),
@@ -118,30 +116,30 @@ setMethod("as.1matrix","real_symmetric_matrix",function(x,drop=TRUE){
          )
 }
 
-`rsm_arith_numeric` <- function(e1,e2){
+`rsm_arith_numeric` <- function(e1, e2){
   switch(.Generic,
-         "+" = jordan_plus_numeric(e1, e2),  
-         "-" = jordan_plus_numeric(e1,-e2),  
-         "*" = jordan_prod_numeric(e1, e2),
+         "+" = jordan_plus_numeric(e1,  e2 ),  
+         "-" = jordan_plus_numeric(e1, -e2 ),  
+         "*" = jordan_prod_numeric(e1,  e2 ),
          "/" = jordan_prod_numeric(e1, 1/e2),
-         "^" = rsm_power_numeric(e1, e2),
+         "^" = rsm_power_numeric  (e1,  e2 ),
          stop(gettextf("binary operator %s not defined for rsm objects", dQuote(.Generic)))
          )
 }
 
-`numeric_arith_rsm` <- function(e1,e2){
+`numeric_arith_rsm` <- function(e1, e2){
   switch(.Generic,
-         "+" = jordan_plus_numeric(e2, e1),  
-         "-" = jordan_plus_numeric(-e2,e1),  
-         "*" = jordan_prod_numeric(e2, e1),
+         "+" = jordan_plus_numeric( e2, e1),  
+         "-" = jordan_plus_numeric(-e2, e1),  
+         "*" = jordan_prod_numeric( e2, e1),
          "/" = jordan_prod_numeric(rsm_inverse(e2),e1),
-         "^" = jordan_power_jordan(e2, e1),
+         "^" = jordan_power_jordan( e2, e1),
          stop(gettextf("binary operator %s not defined for rsm objects", dQuote(.Generic)))
          )
 }
 
-setMethod("Arith",signature(e1 = "real_symmetric_matrix", e2="missing"),
-          function(e1,e2){
+setMethod("Arith", signature(e1 = "real_symmetric_matrix", e2="missing"),
+          function(e1, e2){
             switch(.Generic,
                    "+" = e1,
                    "-" = jordan_negative(e1),
@@ -149,28 +147,37 @@ setMethod("Arith",signature(e1 = "real_symmetric_matrix", e2="missing"),
                    )
           } )
 
-setMethod("Arith",signature(e1="real_symmetric_matrix" ,e2="real_symmetric_matrix"),    rsm_arith_rsm    )
-setMethod("Arith",signature(e1="real_symmetric_matrix" ,e2="numeric"              ),    rsm_arith_numeric)
-setMethod("Arith",signature(e1="numeric"               ,e2="real_symmetric_matrix"),numeric_arith_rsm    )
+setMethod("Arith", signature(e1="real_symmetric_matrix" , e2="real_symmetric_matrix"),    rsm_arith_rsm    )
+setMethod("Arith", signature(e1="real_symmetric_matrix" , e2="numeric"              ),    rsm_arith_numeric)
+setMethod("Arith", signature(e1="numeric"               , e2="real_symmetric_matrix"),numeric_arith_rsm    )
 
-setMethod("[",signature(x="real_symmetric_matrix",i="index",j="missing",drop="logical"),
-          function(x,i,j,drop){
-              out <- as.matrix(x)[,i,drop=FALSE]
+setMethod("[",
+          signature(
+              x = "real_symmetric_matrix",
+              i = "index", j = "missing",
+              drop="logical"
+          ),
+          function(x, i, j, drop){
+              out <- as.matrix(x)[, i, drop=FALSE]
               if(drop){
-                  if(ncol(out)==1){
+                  if(ncol(out) == 1){
                       return(vec_to_rsm1(c(out)))
                   } else {
                       stop("for >1 element, use as.list()")
                   } 
               } else {
-                  return(as.jordan(out,x))
+                  return(as.jordan(out, x))
               }
           } )
               
-setReplaceMethod("[",signature(x="real_symmetric_matrix",i="index",j="missing",value="real_symmetric_matrix"),
-                 function(x,i,j,value){
-                   out <- as.matrix(x)
-                   out[,i] <- as.matrix(value)  # the meat
-                   return(as.jordan(out,x))
+setReplaceMethod("[",
+                 signature(
+                     x = "real_symmetric_matrix",
+                     i = "index", j = "missing",
+                     value = "real_symmetric_matrix"
+                 ),
+                 function(x, i, j, value){
+                     out <- as.matrix(x)
+                     out[,i] <- as.matrix(value)  # the meat
+                     return(as.jordan(out, x))
                  } )
-
--- R/type_2.R
+++ R/type_2.R
@@ -1,7 +1,7 @@
 ## complex Hermitian matrices ("chm"); setClass("complex_herm_matrix") in 'aaa_allclasses.R'
 
-`complex_herm_matrix` <- function(M){new("complex_herm_matrix",x=cbind(M))}  # this is the only place new("real_symmetric_matrix",...) is called
-`is.complex_herm_matrix` <- function(x){inherits(x,"complex_herm_matrix")}
+`complex_herm_matrix` <- function(M){new("complex_herm_matrix", x=cbind(M))}  # this is the only place new("real_symmetric_matrix",...) is called
+`is.complex_herm_matrix` <- function(x){inherits(x, "complex_herm_matrix")}
 
 `r_to_n_chm` <- function(r){sqrt(r)}
 `n_to_r_chm` <- function(n){n^2}
@@ -30,7 +30,7 @@
 
 setValidity("complex_herm_matrix", valid_chm)
 
-`as.complex_herm_matrix` <- function(x,d,single=FALSE){  # single modelled on as.onion()
+`as.complex_herm_matrix` <- function(x, d, single=FALSE){  # single modelled on as.onion()
     if(is.complex_herm_matrix(x)){
         return(x)
     } else if(is.matrix(x)){
@@ -39,14 +39,14 @@ setValidity("complex_herm_matrix", valid_chm)
         if(single){
             return(complex_herm_matrix(x))
         } else {
-            return(numeric_to_complex_herm_matrix(x,d)) # problem! we do not know how big it is
+            return(numeric_to_complex_herm_matrix(x, d)) # problem! we do not know how big it is
         }
     } else {
         stop("not recognised")
     }
 }
 
-`numeric_to_complex_herm_matrix` <- function(x,d){stop("no unique coercion")}
+`numeric_to_complex_herm_matrix` <- function(x, d){stop("no unique coercion")}
 
 `rchm` <- function(n=3,d=5){complex_herm_matrix(matrix(round(rnorm(n*(d*d)),2),ncol=n))}
 `chm_id` <- function(n,d){as.complex_herm_matrix(kronecker(chm1_to_vec(diag(nrow=d)),t(rep(1,n))))}
@@ -55,7 +55,7 @@ setValidity("complex_herm_matrix", valid_chm)
    r <- length(x)
    n <- sqrt(r)
    stopifnot(n==round(n))
-   out <- matrix(0i,n,n)
+   out <- matrix(0i, n, n)
    out[upper.tri(out,FALSE)] <- x[(n+1):(n*(n+1)/2)] + 1i*x[(n*(n+1)/2+1):(n^2)]
    out <- out + ht(out)
    diag(out) <- x[seq_len(n)]
@@ -65,28 +65,28 @@ setValidity("complex_herm_matrix", valid_chm)
 `chm1_to_vec` <- function(M){
     c(
         Re(diag(M)),
-        Re(M[upper.tri(M,FALSE)]),
-        Im(M[upper.tri(M,FALSE)])
+        Re(M[upper.tri(M, FALSE)]),
+        Im(M[upper.tri(M, FALSE)])
     )
 }
 
 `vec_chmprod_vec` <- function(x,y){
     x <- vec_to_chm1(x)
     y <- vec_to_chm1(y)
-    chm1_to_vec((cprod(x,y)+cprod(y,x))/2)
+    chm1_to_vec((cprod(x,y) + cprod(y,x))/2)
 }
 
-setMethod("as.1matrix","complex_herm_matrix",function(x,drop=TRUE){
-    out <- lapply(seq_along(x), function(i){x[i,drop=TRUE]})
-    if((length(x)==1) & drop){out <- out[[1]]}
+setMethod("as.1matrix","complex_herm_matrix",function(x, drop=TRUE){
+    out <- lapply(seq_along(x), function(i){x[i, drop=TRUE]})
+    if((length(x) == 1) & drop){out <- out[[1]]}
     return(out)
 } )
 
-`chm_prod_chm`  <- function(e1,e2){
-    jj <- harmonize_oo(e1,e2)
+`chm_prod_chm`  <- function(e1, e2){
+    jj <- harmonize_oo(e1, e2)
     out <- jj[[1]]*0
     for(i in seq_len(ncol(out))){
-        out[,i] <- vec_chmprod_vec(jj[[1]][,i],jj[[2]][,i])
+        out[,i] <- vec_chmprod_vec(jj[[1]][,i], jj[[2]][,i])
     }
     return(as.jordan(out,e1))
 }
@@ -94,22 +94,22 @@ setMethod("as.1matrix","complex_herm_matrix",function(x,drop=TRUE){
 `chm_inverse` <- function(e1){
     out <- as.matrix(e1)
     for(i in seq_len(ncol(out))){
-        out[,i] <- chm1_to_vec(solve(e1[i,drop=TRUE])) # the meat
+        out[,i] <- chm1_to_vec(solve(e1[i, drop=TRUE])) # the meat
     }
-    return(as.jordan(out,e1))
+    return(as.jordan(out, e1))
 }
 
-`chm_power_numeric` <- function(e1,e2){
-    jj <- harmonize_oo(e1,e2)
+`chm_power_numeric` <- function(e1, e2){
+    jj <- harmonize_oo(e1, e2)
     out <- jj[[1]]*0
     for(i in seq_len(ncol(out))){
-        out[,i] <- chm1_to_vec(mymatrixpower(vec_to_chm1(jj[[1]][,i]),jj[[2]][i])) # the meat
+        out[,i] <- chm1_to_vec(mymatrixpower(vec_to_chm1(jj[[1]][,i]), jj[[2]][i])) # the meat
     }
     return(as.jordan(out,e1))
 }
 
 
-`chm_arith_chm` <- function(e1,e2){
+`chm_arith_chm` <- function(e1, e2){
   switch(.Generic,
          "+" = jordan_plus_jordan(e1, e2),
          "-" = jordan_plus_jordan(e1,jordan_negative(e2)),
@@ -120,7 +120,7 @@ setMethod("as.1matrix","complex_herm_matrix",function(x,drop=TRUE){
          )
 }
 
-`chm_arith_numeric` <- function(e1,e2){
+`chm_arith_numeric` <- function(e1, e2){
   switch(.Generic,
          "+" = jordan_plus_numeric(e1, e2),  
          "-" = jordan_plus_numeric(e1,-e2),  
@@ -131,7 +131,7 @@ setMethod("as.1matrix","complex_herm_matrix",function(x,drop=TRUE){
          )
 }
 
-`numeric_arith_chm` <- function(e1,e2){
+`numeric_arith_chm` <- function(e1, e2){
   switch(.Generic,
          "+" = jordan_plus_numeric(e2, e1),  
          "-" = jordan_plus_numeric(-e2,e1),  
@@ -143,7 +143,7 @@ setMethod("as.1matrix","complex_herm_matrix",function(x,drop=TRUE){
 }
 
 setMethod("Arith",signature(e1 = "complex_herm_matrix", e2="missing"),
-          function(e1,e2){
+          function(e1, e2){
             switch(.Generic,
                    "+" = e1,
                    "-" = jordan_negative(e1),
@@ -151,29 +151,42 @@ setMethod("Arith",signature(e1 = "complex_herm_matrix", e2="missing"),
                    )
           } )
 
-setMethod("Arith",signature(e1="complex_herm_matrix",e2="complex_herm_matrix"),    chm_arith_chm    )
-setMethod("Arith",signature(e1="complex_herm_matrix",e2="numeric"            ),    chm_arith_numeric)
-setMethod("Arith",signature(e1="numeric"            ,e2="complex_herm_matrix"),numeric_arith_chm    )
+setMethod("Arith",signature(e1="complex_herm_matrix", e2="complex_herm_matrix"),     chm_arith_chm    )
+setMethod("Arith",signature(e1="complex_herm_matrix", e2="numeric"            ),     chm_arith_numeric)
+setMethod("Arith",signature(e1="numeric"            , e2="complex_herm_matrix"), numeric_arith_chm    )
 
-setMethod("[",signature(x="complex_herm_matrix",i="index",j="missing",drop="logical"),
-          function(x,i,j,drop){
-              out <- as.matrix(x)[,i,drop=FALSE]
+setMethod("[", signature(x="complex_herm_matrix", i="index", j="missing", drop="logical"),
+          function(x, i, j, drop){
+              out <- as.matrix(x)[, i, drop=FALSE]
               if(drop){
-                  if(ncol(out)==1){
+                  if(ncol(out) == 1){
                       return(vec_to_chm1(c(out)))
                   } else {
                       stop("for >1 element, use as.list()")
                   } 
               } else {
-                  return(as.jordan(out,x))
+                  return(as.jordan(out, x))
               }
           } )
               
-setReplaceMethod("[",signature(x="complex_herm_matrix",i="index",j="missing",value="complex_herm_matrix"),  # matches rsm equivalent
-                 function(x,i,j,value){
-                   out <- as.matrix(x)
-                   out[,i] <- as.matrix(value)  # the meat
-                   return(as.jordan(out,x))
+setReplaceMethod("[",
+                 signature(
+                     x = "complex_herm_matrix",
+                     i = "index", j = "missing",
+                     value = "complex_herm_matrix"
+                 ),  # matches rsm equivalent
+                 function(x, i, j, value){
+                     out <- as.matrix(x)
+                     out[,i] <- as.matrix(value)  # the meat
+                     return(as.jordan(out,x))
                  } )
 
-setReplaceMethod("[",signature(x="complex_herm_matrix",i="index",j="ANY",value="ANY"),function(x,i,j,value){stop("second argument redundant")})  # matches rsm equivalent
+setReplaceMethod("[",
+                 signature(
+                     x = "complex_herm_matrix",
+                     i = "index", j = "ANY",
+                     value="ANY"
+                 ),
+                 function(x, i, j, value){
+                     stop("second argument redundant")
+                 } )  # matches rsm equivalent
